2fcbdedcda9f2abbe81ef2232640194f1ce3b9f0
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2019-02-19 15:55:46 +0100

    Align workshop for foss4g-it

--- presentation/index.js
+++ presentation/index.js
@@ -11,36 +11,50 @@ import {
     List,
     Quote,
     Slide,
-    Text
+    Text,
+    CodePane,
+    Code,
+    Link,
+    Image
 } from "spectacle";
+
+import Terminal from "spectacle-terminal";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
 import CodeSlide from 'spectacle-code-slide';
+import preloader from "spectacle/lib/utils/preloader";
 
 // Require CSS
 require("normalize.css");
+
+const images = {
+    MapReacterImage: require("../assets/mapreacter.png"),
+    GeobeyondImage: require("../assets/gb-logo.png")
+};
+preloader(images);
 
 const theme = createTheme({
     primary: "white",
     secondary: "#1F2022",
     tertiary: "#03A9FC",
-    quarternary: "#CECECE"
+    quarternary: "#CECECE",
+    codeslide: "#122b45"
 }, {
-        primary: "Montserrat",
-        secondary: "Helvetica"
-    });
+    primary: "Montserrat",
+    secondary: "Helvetica"
+});
 
 export default class Presentation extends React.Component {
     render() {
         return (
             <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
                 <Slide transition={["zoom"]} bgColor="primary">
-                    <Heading size={1} fit caps lineHeight={1} textColor="secondary">React, Redux e BoundlessSDK</Heading>
-                    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-                        Marco Montanari & Francesco Bartoli
-                    </Text>
+                    <Heading size={1} fit caps lineHeight={1} textColor="secondary">React, Redux e Boundless WebSDK</Heading>
+                    <Heading margin="10px 0 0" textColor="tertiary" size={5} bold>
+                        Francesco Bartoli
+                    </Heading>
                 </Slide>
 
                 <Slide transition={["fade"]} bgColor="tertiary">
@@ -54,10 +68,12 @@ export default class Presentation extends React.Component {
                 <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
                     <Heading size={6} textColor="secondary" caps>Il bello di React</Heading>
                     <List>
-                        <ListItem>Velocissimo a fare quello che fa bene (manipolazione del DOM)</ListItem>
+                        <ListItem>Velocissimo a fare quello che sa far bene (manipolazione del DOM)</ListItem>
                         <ListItem>Ciclo di vita dei componenti semplice</ListItem>
                         <ListItem>Facile trasformare siti "classici" in applicazioni React</ListItem>
-                        <ListItem>Ha un ottimo generatore <pre></pre></ListItem>
+                        <ListItem>Ha un ottimo generatore</ListItem>
+                        <ListItem>Usa la sintassi JSX</ListItem>
+                        <ListItem>E' possibile comporre insieme piu' componenti</ListItem>
                     </List>
                 </Slide>
 
@@ -72,7 +88,7 @@ export default class Presentation extends React.Component {
                 <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
                     <Heading size={6} textColor="secondary" caps>Il bello di Redux</Heading>
                     <List>
-                        <ListItem>Centralizza lo stato</ListItem>
+                        <ListItem>Centralizza lo stato gestendo "state", "actions" e "reducers"</ListItem>
                         <ListItem>Gestisce in modo lineare il flusso di cambiamenti di stato</ListItem>
                         <ListItem>Facilita il debugging</ListItem>
                         <ListItem>Consente di strutturare operazioni gradualmente pi&ugrave; complesse</ListItem>
@@ -80,15 +96,15 @@ export default class Presentation extends React.Component {
                 </Slide >
 
                 <Slide transition={["fade"]} bgColor="tertiary">
-                    <Heading size={6} textColor="primary" caps>BoundlessSDK...</Heading>
+                    <Heading size={6} textColor="primary" caps>Boundless WebSDK...</Heading>
                     <BlockQuote>
                         <Quote> Javascript SDK based on React, OpenLayers and Redux. </Quote>
-                        <Cite> <a href="https://github.com/boundlessgeo/sdk"> BoundlessSDK </a></Cite>
+                        <Cite> <a href="https://github.com/boundlessgeo/sdk"> Boundless WebSDK </a></Cite>
                     </BlockQuote>
                 </Slide>
 
                 <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-                    <Heading size={6} textColor="secondary" caps>Il bello di BoundlessSDK</Heading>
+                    <Heading size={6} textColor="secondary" caps>Il bello di Boundless WebSDK</Heading>
                     <List>
                         <ListItem>NON dobbiamo pi&ugrave; inizializzare OL</ListItem>
                         <ListItem>Le operazioni non sono pi&ugrave; "grafiche" ma strutturali</ListItem>
@@ -103,55 +119,60 @@ export default class Presentation extends React.Component {
                 </Slide>
 
 
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={6} textColor="primary" caps>Prepariamo l'app</Heading>
-                    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-                        npm install -g create-react-app<br />
-                        create-react-app ./boundlessdemo<br />
-                        cd boundlessdemo<br />
-                    </Text>
+                <Slide transition={[ "spin", "slide" ]} bgColor="primary">
+                    <Heading size={ 6 } caps fit textColor="tertiary">Prepariamo l'app</Heading>
+                    <Terminal title="1. foss4g-it@foss4g-it: ~(zsh)" output={[
+                        "$ npm install -g yarn",
+                        "$ npm install -g create-react-app",
+                        "$ create-react-app ./boundless-sdk-demo",
+                        "$ yarn start"
+                    ]}
+                    />
                 </Slide>
 
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={6} textColor="primary" caps>Installiamo il necessario</Heading>
-                    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-                        yarn add node-sass-chokidar<br />
-                        yarn add redux react-redux<br />
-                        yarn add ol ol-mapbox-style<br />
-                        yarn add @boundlessgeo/sdk<br />
-                        mv src/App.css src/App.scss<br />
-                    </Text>
+                <Slide transition={[ "spin", "slide" ]} bgColor="primary">
+                    <Heading size={ 6 } caps fit textColor="tertiary">Installiamo il necessario</Heading>
+                    <Terminal title="1. foss4g-it@foss4g-it: ~(zsh)" output={[
+                        "$ yarn add redux react-redux redux-thunk",
+                        "$ yarn add @boundlessgeo/sdk",
+                        "$ yarn add ol ol-mapbox-style",
+                        "$ yarn add babel-core babel-cli",
+                        "$ yarn add style-loader css-loader",
+                        "$ yarn add babel-preset-env babel-preset-react babel-loader",
+                        "$ yarn start"
+                    ]}
+                    />
                 </Slide>
 
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={6} textColor="primary" caps>Cambiamo packages.json</Heading>
-                    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-                        Aggiungiamo a "scripts":...<br /><br />
-                        "build-css": "node-sass-chokidar src/ --include-path node_modules/ -o src/",<br />
-                        "watch-css": "npm run build-css && node-sass-chokidar src/ --include-path node_modules/ -o src/ --watch --recursive",<br />
-                    </Text>
-                </Slide>
-
-                <CodeSlide transition={["fade"]} bgColor="secondary"
-                    lang="js"
+                <CodeSlide transition={["fade"]} bgColor="codeslide"
+                    lang="jsx"
                     code={require("raw-loader!../assets/step1.example")}
                     ranges={[
-                      { loc: [0, 270], title: "Scriviamo l'app!" },
-                      { loc: [0, 1], note: "Importiamo redux" },
-                      { loc: [2, 5], note: "Importiamo Boundless"  },
-                      { loc: [6, 9], note: "Inseriamo il reducer" },
-                      { loc: [10, 12], note: "Ascoltiamo il componentDidMount del lifecycle" },
-                      { loc: [12, 21], note: "Aggiungiamo una sorgente" },
-                      { loc: [23, 28], note: "Aggiungiamo un layer" },
-                      { loc: [33, 34], note: "Inseriamo la mappa nella pagina" },
+                        { loc: [0, 63], title: "Scriviamo l'app!" },
+                        { loc: [3, 5], note: "Importiamo redux" },
+                        { loc: [5, 8], note: "Importiamo Boundless"  },
+                        { loc: [9, 12], note: "Inseriamo il reducer nello store Redux" },
+                        { loc: [15, 16], note: "Ascoltiamo il componentDidMount del lifecycle" },
+                        { loc: [16, 26], note: "Aggiungiamo una sorgente" },
+                        { loc: [27, 32], note: "Aggiungiamo un layer" },
+                        { loc: [51, 56], note: "Inseriamo la mappa nella pagina" },
+                        { loc: [63, 63], title: "Step 1 completato!", note: (
+                            <span>Git repo here: <a href="https://github.com/geobeyond/boundless-sdk-demo/tree/step-1">geobeyond/boundless-sdk-demo/tree/step-1</a></span>
+                        )},
                     ]} />
 
                 <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-                    <Heading size={6} textColor="secondary" caps>Cosa offre BoundlessSDK?</Heading>
+                    <Heading size={6} textColor="secondary" caps>Cosa offre Boundless WebSDK?</Heading>
                     <List>
-                        <ListItem>Actions e Reducers REDUX (drawing, map, mapbox, print, wfs)</ListItem>
-                        <ListItem>Componenti (map, history, layer-list, legend, scaleline, zoom-control,
-                            zoom-slider, wfs</ListItem>
+                        <ListItem>Actions e Reducers REDUX:</ListItem>
+                            <List textColor="secondary">
+                                <ListItem margin={50} textSize={35}>drawing, mapbox, print, wfs, etc</ListItem>
+                            </List>
+                        <ListItem>Componenti:</ListItem>
+                            <List textColor="secondary">
+                                <ListItem margin={50} textSize={35}>map, history, layer-list, legend, scaleline, zoom-control,
+                            zoom-slider, wfs, etc</ListItem>
+                            </List>
                         <ListItem><a href="https://sdk.boundlessgeo.com/docs/">Documentazione</a></ListItem>
                     </List>
                 </Slide>
@@ -165,32 +186,107 @@ export default class Presentation extends React.Component {
                     </List>
                 </Slide>
 
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-                        Torniamo sul codice e aggiungiamo un layerswitcher
-                    </Text>
+                <Slide transition={[ "spin", "slide" ]} bgColor="primary">
+                    <Heading size={ 6 } caps fit textColor="tertiary">Ci manca una libreria...per sass</Heading>
+                    <Terminal title="1. foss4g-it@foss4g-it: ~(zsh)" output={[
+                        "$ yarn add node-sass",
+                        "$ yarn start"
+                    ]}
+                    />
                 </Slide>
 
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={6} textColor="secondary" caps>Ci manca una libreria...</Heading>
-                    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-                        yarn add react-dnd
-                    </Text>
-                </Slide>
-
-                <CodeSlide transition={["fade"]} bgColor="secondary"
-                    lang="js"
+                <CodeSlide transition={["fade"]} bgColor="codeslide"
+                    lang="jsx"
                     code={require("raw-loader!../assets/step2.example")}
                     ranges={[
-                        { loc: [0, 270], title: "Aggiungiamo un layerswitcher" },
-                        { loc: [6, 8], note: "Importiamo SdkLayerList e Item" },
-                        { loc: [9, 10], note: "Importiamo Provider" },
-                        { loc: [11, 12], note: "Importiamo ReactDND" },
-                        { loc: [17, 30], note: "Creiamo il visualizzatore del singolo elemento" },
-                        { loc: [31, 32], note: "Lo rendiamo drag-droppable" },
-                        { loc: [58, 61], note: "E lo inseriamo nella pagina" },
-
+                        { loc: [0, 73], title: "Aggiungiamo zoom e suo slider" },
+                        { loc: [6, 8], note: "Importiamo lo zoom con il suo slider" },
+                        { loc: [13, 16], note: "Dichiariamo le estensioni di sviluppo per Redux"  },
+                        { loc: [21, 22], note: "Impostiamo il centro mappa sull'Italia" },
+                        { loc: [60, 64], note: "Aggiungiamo i componenti nella mappa" },
+                        { loc: [73, 73], title: "Step 2 completato!", note: (
+                            <span>Git repo here: <a href="https://github.com/geobeyond/boundless-sdk-demo/tree/step-2">geobeyond/boundless-sdk-demo/tree/step-2</a></span>
+                        )},
                     ]} />
+
+                <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+                    <Heading size={6} textColor="secondary" caps>Aggiungiamo un dato vettoriale</Heading>
+                    <List>
+                        <ListItem>Vogliamo utilizzare un layer vettoriale</ListItem>
+                        <ListItem>Riutilizzabile e disponibile online, per esempio su Padova</ListItem>
+                        <ListItem>Pubblicato in formato GeoJSON</ListItem>
+                        <ListItem>Open Data su Padova in GitHub by <Link href="https://github.com/peterampazzo/OpenData-Padova">Pietro Rampazzo</Link>: <Link href="https://raw.githubusercontent.com/peterampazzo/OpenData-Padova/master/geojson/varchi.geojson">layer varchi</Link></ListItem>
+                    </List>
+                </Slide>
+
+                <CodeSlide transition={["fade"]} bgColor="codeslide"
+                    lang="jsx"
+                    code={require("raw-loader!../assets/step3.example")}
+                    ranges={[
+                        { loc: [0, 145], title: "Aggiungiamo un layer GeoJSON" },
+                        { loc: [66, 71], note: (<span>Aggiungiamo il tipo di sorgente GeoJSON allo store, notare la URL in data - vedi <a href="https://gist.github.com/jesperorb/6ca596217c8dfba237744966c2b5ab1e">CORS</a></span>) },
+                        { loc: [72, 83], note: "Dichiariamo il layer varchi nello store come puntuale" },
+                        { loc: [9, 10], title: "Aggiungiamo un popup", note: "Importiamo il componente di popup" },
+                        { loc: [21, 42], note: "Creiamo un popup custom per le nostre feature" },
+                        { loc: [104, 110], note: "Aggiungiamo l'evento onClick con una Promise" },
+                        { loc: [113, 121], note: "Raccogliamo le informazioni delle feature selezionate" },
+                        { loc: [121, 130], note: "Aggiungiamo il componente alla mappa" },
+                        { loc: [145, 145], title: "Step 3 completato!", note: (
+                            <span>Git repo here: <a href="https://github.com/geobeyond/boundless-sdk-demo/tree/step-3">geobeyond/boundless-sdk-demo/tree/step-3</a></span>
+                        )},
+                    ]} />
+
+                <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+                    <Heading size={6} textColor="secondary" caps>Aggiungiamo un WMS da GeoServer</Heading>
+                    <List>
+                        <ListItem>Vogliamo utilizzare un layer WMS pubblicato da un GeoServer</ListItem>
+                        <ListItem>Riutilizziamo il classico "states" dal GeoServer di Boundless</ListItem>
+                        <ListItem>Prima però vogliamo aggiungere l'attribuzione a OpenStreetMap</ListItem>
+                    </List>
+                </Slide>
+
+                <CodeSlide transition={["fade"]} bgColor="codeslide"
+                    lang="jsx"
+                    code={require("raw-loader!../assets/step4.example")}
+                    ranges={[
+                        { loc: [0, 162], title: "Aggiungiamo un layer WMS" },
+                        { loc: [8, 9], note: "Inseriamo il componente scaleline" },
+                        { loc: [54, 55], note: "Inseriamo la proprietà nella sorgente osm dello store" },
+                        { loc: [149, 150], note: "Aggiungiamo il componente alla mappa" },
+                        { loc: [85, 91], note: "Dichiariamo la sorgente WMS di tipo raster nello store" },
+                        { loc: [92, 99], title: "Aggiungiamo il layer WMS allo store" },
+                        { loc: [145, 145], title: "Step 4 completato!", note: (
+                            <span>Git repo here: <a href="https://github.com/geobeyond/boundless-sdk-demo/tree/step-4">geobeyond/boundless-sdk-demo/tree/step-4</a></span>
+                        )},
+                    ]} />
+
+                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+                        Lasciamo la gestione del popup tra i due differenti layer come esercizio
+                    </Text>
+                </Slide>
+
+                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+                        Dove possiamo andare da qui?
+                    </Text>
+                </Slide>
+
+                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                    <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+                        Mapreacter: un'applicazione complessa
+                    </Text>
+                    <Image width="100%" src={images.MapReacterImage} />
+                    <Link href="https://github.com/geobeyond/mapreacter">GitHub repo</Link>
+                </Slide>
+
+                <Slide transition={["fade"]} bgColor="primary" textColor="primary">
+                    <Text margin="10px 0 0" textColor="tertiary" size={2} fit bold>
+                        Grazie dell'attenzione
+                    </Text>
+                    <Image width="30%" src={images.GeobeyondImage} />
+                    <Link textColor="tertiary" href="mailto:info@geobeyond.it" bold>info@geobeyond.it</Link>
+                </Slide>
 
             </Deck>
         );
